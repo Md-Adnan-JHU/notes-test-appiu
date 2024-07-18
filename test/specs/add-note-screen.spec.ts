b17e0b6daf9f7d0db9e14ba5505cb1f616f65ec9
Md-Adnan-JHU <adnanjhdelhi@gmail.com>
2024-07-18 18:52:16 +0530

    add polyfill

--- test/specs/add-note-screen.spec.ts
+++ test/specs/add-note-screen.spec.ts
@@ -1,3 +1,10 @@
+// Polyfill for Object.hasOwn
+if (!Object.hasOwn) {
+  Object.hasOwn = function(obj, prop) {
+      return Object.prototype.hasOwnProperty.call(obj, prop);
+  };
+}
+
 import  { AddNoteScreen }  from "../screenObject/add-note.screen";
 
 describe('Add Notes', () => {
--- test/specs/delete-note-screen.spec.ts
+++ test/specs/delete-note-screen.spec.ts
@@ -1,3 +1,11 @@
+// Polyfill for Object.hasOwn
+if (!Object.hasOwn) {
+  Object.hasOwn = function(obj, prop) {
+      return Object.prototype.hasOwnProperty.call(obj, prop);
+  };
+}
+
+
 import  { EditNoteScreen }  from "../screenObject/edit-note.screen";
 
 
